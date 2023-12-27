50025bb50af4db5be7441885bda21e309c2999cf
Christian Payne <paynechristian@outlook.com>
2023-12-27 13:13:40 +0000

    Adding setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Loess-models',
+    version='1.0.0',
+    url='https://github.com/ChrisPayneHome/Loess-model.git',
+    author='Christian Payne',
+    author_email='',
+    description='Implementation of Loess model',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
